2570e8d774780e107da58550662bc99bf70355a6
Peter Busatti <peterbusatti@gmail.com>
2021-11-10 17:56:10 -0500

    add funtionality to ui and lets player play game from ui

--- app.js
+++ app.js
@@ -7,89 +7,78 @@ function computerPlay() {
     return CHOICE[Math.floor(Math.random() * CHOICE.length)];
 }
 
-/* create a function to play a single round, takes two parameters: playerSelection
-and computerSelection.
-if same choice return a tie. 
-need cases for 3 player choices (r,p,s) with each case having two outcomes: win or
-lose.
-*/ 
-let playerSelection; 
+const playerOutput = document.getElementById("player-output");
+const cpuOutput = document.getElementById("cpu-output");
+const container = document.getElementById("container");
+const btn = document.querySelectorAll("input");
+const logDisplay = document.getElementById("log-display");
+const replayButton = document.getElementById("reset-btn");
 
-// function to get user prompt and basic input validation
-function playerInput() {
-    let playerChoice = window.prompt("Choose and type Rock, Paper or Scissors");
-    playerChoice = playerChoice.toLowerCase();
-    while (!(playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors")) {
-        playerChoice = window.prompt("Please ONLY enter Rock, Paper or Scissors");
-        playerChoice = playerChoice.toLowerCase();
-    }
-    playerSelection = playerChoice;  
-}
+let playerScore = 0;
+let cpuScore = 0;
 
-// function for ONE round of play, includes solution to avoid case sensitivity
+btn.forEach(input => {input.addEventListener("mouseenter", addBorder)} );
+btn.forEach(input => {input.addEventListener("mouseleave", removeBorder)} );
+btn.forEach(input => {input.addEventListener("click", getPlayerChoice)} );
 
-// keeping both boolean variable global to reference them again
-let win = Boolean(false); // if true player is winner, if false comp is winner
-let tie = Boolean(false); // if true there is a tie, if false no tie
+replayButton.addEventListener("click", () => location.reload());
 
 function playRound(playerSelection, computerSelection) {
+    logDisplay.style.display = "block";
     // case insensitivity
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     // case of tie
     if (playerSelection == computerSelection) {
-        tie = true; // setting tie boolean to true
-        return "Tie! You both chose " + playerSelection;
+        logDisplay.textContent = "Tie! You both chose " + playerSelection;
     }
     // cases of loss
     else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" ||
               playerSelection === "scissors" && computerSelection === "rock") {
-        tie = false; // setting tie boolean to false
-        win = false; // false = player loss
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        cpuScore++;
+        cpuOutput.textContent = cpuScore;
+        logDisplay.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
     }
     // cases of win
     else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection == "rock" ||
               playerSelection === "scissors" && computerSelection === "paper") {
-        tie = false;
-        win = true;
-        return "You Win! " + playerSelection + " beats " + computerSelection;
+        playerScore++;
+        playerOutput.textContent = playerScore;
+        logDisplay.textContent = "You Win! " + playerSelection + " beats " + computerSelection;  
     }
+    checkWin();
 }
 
-// function to play a game to a score of 5 using a while loop
-function game() {
-    i = 0; // player win counter
-    j = 0; // computer win counter
-    while ((i < 5 && j != 5) || (j < 5 && i != 5)) { // i = player counter , j = computer counter
-        playerInput();
-        playRound(playerSelection, computerPlay());
-        console.log(playRound(playerSelection, computerPlay()));
-        
-        if (tie) {
-            //console.log("tie");
-            i = i;
-            j = j;
-            console.log("player " + i);
-            console.log("comp " + j);
-        }
-        else if (!(tie)) {
-            if (win) {
-                i++;
-            }
-            else if (!(win)) {
-                j++;
-            }
-        console.log("player " + i);
-        console.log("comp " + j);
-        }
+function addBorder(e) {
+    e.target.className = "btn-hover";
+}
+
+function removeBorder(e) {
+    e.target.className = "btn";
+}
+
+function getPlayerChoice(e) {
+    let playerChoice = e.target.id;
+    playRound(playerChoice, computerPlay());
+    console.log(playerScore);
+    console.log(cpuScore);
+}
+
+function checkWin () {
+    if (playerScore === 5 || cpuScore === 5) {
+        btn.forEach(input => {
+            input.removeEventListener("click", getPlayerChoice);
+        });
+        replayButton.style.backgroundColor = "red";
+        replayButton.style.fontSize = "35px";
     }
     
-    if (i == 5) {
-        return "Player wins";
-    }
-    else if (j == 5) {
-        return "Computer wins";
+    if (playerScore === 5){
+        logDisplay.textContent = "Game Over! Congratulations you WIN!";
+        
+    } 
+    else if (cpuScore === 5){
+        logDisplay.textContent = "Game Over! Sorry you LOSE!";
     }
 }
 
